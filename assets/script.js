d9edc298c870fb8ae2b2b7d2fe05957fd8c98187
addiesallow <a.derkacz@gmail.com>
2024-02-13 23:12:06 +0100

    Added images folder, updates CSS and JS

---
+++ assets/script.js
@@ -0,0 +1,45 @@
+const alternatives = [
+    {text:"", images:"./assets/images/1.gif"},
+    {text:"I promise, it will be unforgettable.", images:"./assets/images/2.gif"},
+    {text:"Think again...", images:"./assets/images/3.gif"},
+    {text:"Come on now, say yes!", images:"./assets/images/4.gif"},
+];
+
+const ohyes = {text:"I knew you would accept!", images:"./assets/images/yes.gif"};
+const title = document.querySelector('.title');
+const text = document.querySelector('.text');
+const cat = document.querySelector('.cat');
+const buttons = document.querySelectorAll('.button');
+const errorButton = document.querySelector('.button__error');
+
+let count = 0;
+function updateDisplay(item) {
+    console.log(item);
+    cat.src = item.images;
+    text.innerHTML = item.text;
+}
+
+errorButton.addEventListener('click', () => {
+    count = 0;
+    updateDisplay(alternatives[count]);
+    buttons.forEach(btn => btn.style.display = 'inline-block');
+    errorButton.style.display = 'none';
+});
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.textContent === 'YES'){
+            updateDisplay(ohyes);
+            buttons.forEach(btn => btn.style.display = 'none');
+        }
+        if (button.textContent === 'NO'){
+            count++;
+            if(count < alternatives.length){
+                updateDisplay(alternatives[count]);
+            } else {
+                buttons.forEach(btn => btn.style.display = 'none');
+                errorButton.style.display = 'inline-block';
+            }
+        }
+    });
+});
